3846bb5dffe0c5cc2b892e788abc167eb135750f
Sr-Van <vanderson.dev1@gmail.com>
2024-02-06 12:56:03 -0300

    FIX: reply iteration & configuration

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -2,12 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 
 import { ActivatedRoute, Router } from '@angular/router';
+import { Posts } from 'src/app/interfaces/posts';
 import { PostService } from 'src/app/services/post.service';
+import { MentionPipe } from 'src/app/pipes/mention.pipe';
 
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MentionPipe],
   templateUrl: './post.component.html',
   styleUrl: './post.component.css'
 })
@@ -16,11 +18,39 @@ export class PostComponent {
   activeRoute = inject(ActivatedRoute)
   postServ = inject(PostService)
   postId: any = 0
-  postArray: any[] = []
+  postArray: Posts[] = []
 
   ngOnInit(){
     this.postId = this.activeRoute.snapshot.paramMap.get('id')
 
     this.postArray = this.postServ.getPost().filter((post: any) => post.id == this.postId)
+
+  }
+
+  /* Remove when backend enters */
+  addReply(reply: any) {
+    const obj = this.postServ.generateObject('reply')
+    obj.reply = reply.value
+    obj.author = "random"
+    obj.date = this.postServ.generateDate()
+    this.postServ.addNewReply(this.postId, obj)
+    reply.value = ""
+  }
+
+  replyUser(user: string, input: any) {
+    input.value = `@${user}`
+  }
+
+  verifyText(letters: any) {
+    const splitLetters = letters.target.value.split("")
+
+    splitLetters.forEach((letter: string, index: number) => {
+      if (letter === '@' && index !== 0) {
+        splitLetters.splice(index, 1)
+        const newStr = splitLetters.join("")
+        letters.target.value = newStr
+      }
+    })
+
   }
 }
