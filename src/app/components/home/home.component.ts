3c1fe15001036e989ee0c71326abe70a53fb8428
Sr-Van <vanderson.dev1@gmail.com>
2024-02-17 18:10:56 -0300

    FEAT: pagination

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { PostService } from 'src/app/services/post.service';
+import { Posts } from 'src/app/interfaces/posts';
 
 @Component({
   selector: 'app-home',
@@ -13,31 +14,74 @@ export class HomeComponent {
   router = inject(Router)
 
   isLoaded: boolean = false
-  posts: any = []
+  posts: Posts[] = []
+  actualPageArr: Posts[] = []
+  actualPag: number = 1
+  pages: number
 
   ngOnInit() {
     this.posts = this.postService.getPost()
-    console.log(this.posts);
 
+    this.pages = this.posts.length <= 5 ? 1 : this.generatePage(this.posts.length)
+
+    this.updatePagePosts(this.posts, this.actualPag, 5)
+
+    this.loadPage(1500)
+  }
+
+  loadPage(time: number) {
+    this.isLoaded = false
     setTimeout(() => {
       this.isLoaded = true
-    }, 1500)
-
+    }, time)
   }
 
   goToPost(id: number) {
     this.router.navigate([`/post/${id}`])
   }
 
+  updatePagePosts(array: Posts[], page: number, page_size: number) {
+    this.actualPageArr = array.slice((page - 1) * page_size, page * page_size)
+    this.loadPage(800)
+  }
+
+  nextPage() {
+    if (this.actualPag === this.pages) {
+      return
+    }
+    this.actualPag++
+    this.updatePagePosts(this.posts, this.actualPag, 5)
+  }
+
+  previousPage() {
+    if (this.actualPag === 1) {
+      return
+    }
+    this.actualPag--
+    this.updatePagePosts(this.posts, this.actualPag, 5)
+  }
+
+  generatePage(length: number){
+    let counter = 0
+    let pages = 1
+    for (let i=0; i <= length; i++) {
+
+      counter++
+
+      if (counter > 5) {
+        pages++
+        counter = 0
+      }
+    }
+
+    return pages
+  }
+
   sortByMostLiked() {
-    this.posts = this.posts.sort((a:any, b: any) => {
+    this.actualPageArr = this.posts.sort((a:any, b: any) => {
       return b.likes - a.likes
-    })
+    }).slice(0, 5)
 
-    setTimeout(() => {
-      this.isLoaded = true
-    }, 1000);
-    this.isLoaded = false
-
+    this.loadPage(1000)
   }
 }
