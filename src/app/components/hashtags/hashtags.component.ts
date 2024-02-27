ac08af4c413d7af67b01c46d812595ddf0419ce7
Sr-Van <vanderson.dev1@gmail.com>
2024-02-27 09:26:14 -0300

    FEAT: styling & methods hashtag component

--- src/app/components/hashtags/hashtags.component.ts
+++ src/app/components/hashtags/hashtags.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Posts } from 'src/app/interfaces/posts';
 import { PostService } from 'src/app/services/post.service';
+import { LoadingComponent } from '../loading/loading.component';
 
 @Component({
   selector: 'app-hashtags',
   standalone: true,
-  imports: [],
+  imports: [LoadingComponent],
   templateUrl: './hashtags.component.html',
   styleUrl: './hashtags.component.css'
 })
@@ -15,12 +16,17 @@ export class HashtagsComponent {
   router = inject(Router)
   activateRoute = inject(ActivatedRoute)
   postService = inject(PostService)
+
   tag: any
   hashtagsArray: Posts[] = []
+
+  isLoad: boolean = false
 
   ngOnInit() {
     this.tag = this.activateRoute.snapshot.paramMap.get('tag')
     this.getHashtagArray(this.postService.posts, this.tag)
+
+    this.loadPage(1500)
   }
 
   getHashtagArray(array: any, tag: string) {
@@ -31,5 +37,16 @@ export class HashtagsComponent {
         }
       });
     })
+  }
+
+  loadPage(time: number) {
+    this.isLoad = false
+    setTimeout(() =>{
+      this.isLoad = true
+    }, time)
+  }
+
+  goToPost(id: number) {
+    this.router.navigate([`/post/${id}`])
   }
 }
