7619b1e97f0f9086769bfdb8d2ca925d6605e0a4
Sr-Van <vanderson.dev1@gmail.com>
2024-03-27 12:37:30 -0300

    FEAT: implementing google auth

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import {  SocialAuthService, SocialUser, GoogleSigninButtonModule, GoogleLoginProvider } from '@abacritt/angularx-social-login';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [GoogleSigninButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  authService = inject(SocialAuthService)
   switchToSigin: boolean = false
+  router = inject(Router)
+
+  user: SocialUser
+  loggedIn: boolean
+
+  ngOnInit() {
+
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+      console.log(user)
+    });
+
+    if (this.loggedIn) this.router.navigate(['/'])
+  }
 }
